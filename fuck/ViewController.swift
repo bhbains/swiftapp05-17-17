b6e9dfd8a6345710bf37cff5be944473204e8ef3
Brian <pepper@Emilys-Robot.local>
2017-05-17 12:59:40 -0700

    first mutha fuckin commit

---
+++ fuck/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  fuck
+//
+//  Created by Brian on 5/15/17.
+//  Copyright © 2017 Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "Stop"
+        }
+        
+    }
+    @IBAction func buttonTapped2(_ sender: Any) {
+        
+        theLabel.text = "Edo sucks"
+        
+        print("Why")
+
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
